c9126bfc025b76c0e00e1f32b7e16d391fc54421
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-11-02 18:27:13 +0100

    edit auth.js

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,5 +1,31 @@
+import fetch from "node-fetch";
+
 export default async function handler(req, res) {
-  return res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`
-  );
+  const code = req.query.code;
+
+  if (!code) {
+    const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
+    return res.redirect(redirectUrl);
+  }
+
+  const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }),
+  });
+
+  const tokenData = await tokenRes.json();
+
+  if (tokenData.error) {
+    return res.status(400).json(tokenData);
+  }
+
+  res.redirect(`/admin?access_token=${tokenData.access_token}`);
 }
