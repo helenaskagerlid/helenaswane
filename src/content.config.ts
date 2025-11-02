93c9245d11f90c184388d1daa7b80b78d8931d68
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-11-02 18:14:46 +0100

    add decap

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,15 +2,6 @@ import { defineCollection, z } from "astro:content";
 
 import { glob } from "astro/loaders";
 
-// const artGallery = defineCollection({
-//   loader: glob({ pattern: ["*.md"], base: "src/content/art-gallery" }),
-//   schema: ({ image }) =>
-//     z.object({
-//       title: z.string(),
-//       image: image(),
-//     }),
-// });
-
 const transformativeCoaching = defineCollection({
   loader: glob({
     pattern: ["*.md"],
